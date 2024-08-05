01e2ad8c17976c8a4b52905ff7a7f9566bf38e43
classicx06 <nxixrules@gmail.com>
2024-08-05 17:18:30 -0500

    1 archivo

--- conexion.js
+++
@@ -1,8 +0,0 @@
-// Create the connection to database BD
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'nuevo_user',
-  });
-
-module.exports = connection;
